5bd5b74fe127e976eb392a22ff6428a1d9f1d4a9
Sami Duac <39534249+SamiJohannes@users.noreply.github.com>
2020-02-13 12:10:49 -0500

    changed code in app.js

--- src/App.js
+++ src/App.js
@@ -1,63 +1,64 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import Card from './Card.js'
-import Button from './Button.js';
-import Link from './Link.js';
-import quotes from './quotes.json';
+import Button from './Button.js'
+import Link from './Link.js'
+import quotes from './quotes.json'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       randomIndex: Math.floor(Math.random() * 10),
       animateIn: true
     }
 
-  this.disCard = this.disCard.bind(this);
-  this.randomQuote = this.randomQuote.bind(this);
+    this.disCard = this.disCard.bind(this)
+    this.randomQuote = this.randomQuote.bind(this)
   }
 
-  disCard() {
+  disCard () {
     this.setState({
       animateIn: !this.state.animateIn
-    });
+    })
   }
 
-  randomQuote() {
-    if(!this.state.animateIn) {
-      let newIndex = 0;
+  randomQuote () {
+    if (!this.state.animateIn) {
+      console.log(this.state.randomIndex)
+      const newIndex = 0
       this.setState({
         randomIndex: newIndex,
         animateIn: true
-      });
+      })
     }
   }
 
-  render() {
+  render () {
     return (
       <div id="quote-box" className="App">
-        <Card 
+        <Card
           id="text"
           animateIn={this.state.animateIn}
           newQuote={this.randomQuote}
           quote={quotes[this.state.randomIndex].quote}
         >
         </Card>
-          <Link
-            id="tweet-quote"
-            href="twitter.com/intent/tweet"
-            target="_blank"
-            rel="noopener noreferrer"
-            label="tweet!"
-          />
-          <Button
-           id="new-quote"
-           onClick={this.disCard}
-           label="Quoteme!"
-          />
+        <Link
+          id="tweet-quote"
+          href="twitter.com/intent/tweet"
+          target="_blank"
+          rel="noopener noreferrer"
+          label="tweet!"
+        />
+        <Button
+          id="new-quote"
+          onClick={this.disCard}
+          label="Quoteme!"
+        />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
