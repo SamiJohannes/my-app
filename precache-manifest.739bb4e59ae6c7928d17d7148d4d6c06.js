29aaa6d0b920905ad86320536946a035a243468b
Sami Duac <39534249+SamiJohannes@users.noreply.github.com>
2020-02-18 12:32:01 -0500

    Updates

---
+++ precache-manifest.739bb4e59ae6c7928d17d7148d4d6c06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8245ff0514beb006b529bf851ab4aa5",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "1adc57f7746c8f073b5f",
+    "url": "/my-app/static/css/main.fca4de50.chunk.css"
+  },
+  {
+    "revision": "3c7e6003e7c6dfd2c1e3",
+    "url": "/my-app/static/js/2.ca10148f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-app/static/js/2.ca10148f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1adc57f7746c8f073b5f",
+    "url": "/my-app/static/js/main.308a6220.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  }
+]);
